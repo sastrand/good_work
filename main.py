3eb1e02de28baf6d00ce05897ab91eeb7320ed46
sascha <sastrand@pdx.edu>
2019-04-17 13:50:21 -0400

    splits doc into strings, keeps punct marks

--- main.py
+++ main.py
@@ -1,9 +1,12 @@
 import regex as re
 
 def read_paper(paper):
-    p = re.split("[.!?;]+", paper)
-    return p
+    p = re.split("([.!?;]*[.!?;])", paper)
+    ps = []
+    for x in range(0, len(p)-1, 2):
+        ps.append((p[x] + p[x+1]).strip())
+    return ps
 
 if __name__ == "__main__":
-    print(read_paper("Hello, there! How's it going? I see you're typing now."))
+    print(read_paper("!Hello, there?! How's it going? I see you're typing now."))
 
