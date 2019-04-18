5d87300ed47bc1557cf051d6cf8a4913e2c341df
sascha <sastrand@pdx.edu>
2019-04-18 13:30:19 -0400

    read papers, save in dict

--- main.py
+++ main.py
@@ -1,12 +1,26 @@
 import regex as re
 
-def read_paper(paper):
+
+def record_sentence(sentence, papers_read):
+    if sentence in papers_read.keys():
+        papers_read[sentence] = papers_read[sentence]+1
+    else:
+        papers_read[sentence] = 1
+
+def read_paper(paper, papers_read):
     p = re.split("([.!?;]*[.!?;])", paper)
-    ps = []
     for x in range(0, len(p)-1, 2):
-        ps.append((p[x] + p[x+1]).strip())
-    return ps
+        record_sentence((p[x] + p[x+1]).strip(), papers_read)
 
 if __name__ == "__main__":
-    print(read_paper("!Hello, there?! How's it going? I see you're typing now."))
+    papers_read = dict()
+    paper0 = "!Hello, there?! How's it going? I see you're typing now."
+    paper1 = ("In order to foster a convivial atmosphere, no computer work"
+              "is allowed in the cafe.")
+    paper2 = "I see you're typing now. I type, too."
+    read_paper(paper0, papers_read)
+    read_paper(paper1, papers_read)
+    read_paper(paper2, papers_read)
+    print(papers_read)
+
 
